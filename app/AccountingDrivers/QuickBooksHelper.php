96e24a8aa83b5ffbd5e7ab59a75734ac575dcc6b
Shahzaib <Shahzaib.2377@gmail.com>
2023-03-30 21:34:10 +0400

    Changed helper location and renamed class.

---
+++ app/AccountingDrivers/QuickBooksHelper.php
@@ -0,0 +1,175 @@
+<?php
+namespace App\AccountingDrivers;
+
+use App\Tokens;
+use Illuminate\Support\Facades\URL;
+use QuickBooksOnline\API\Core\Http\Serialization\XmlObjectSerializer;
+use QuickBooksOnline\API\Core\OAuth\OAuth2\OAuth2AccessToken;
+use QuickBooksOnline\API\DataService\DataService;
+use QuickBooksOnline\API\Facades\Estimate;
+use Symfony\Component\CssSelector\Parser\Token;
+
+class QuickBooksHelper
+{
+    private static $merchantId = 1000;
+
+    /**
+     * Function to get auth url
+     * @return string
+     */
+    public static function getAuthUrl()
+    {
+        $ds = self::getDataService();
+        $loginHelper = $ds->getOAuth2LoginHelper();
+        $authUrl = $loginHelper->getAuthorizationCodeURL();
+        return $authUrl;
+    }
+
+    /**
+     * Function to get access token
+     * @param string $code Obtained code during the 0auth
+     * @param string $realmId Obtained from the app
+     * @return array
+     * @throws \Exception
+     */
+    public static function exchangeCodeForToken($code, $realmId)
+    {
+        if(empty($code) || empty($realmId))
+            throw new \Exception("Invalid token");
+
+        try
+        {
+            $ds = self::getDataService();
+            $loginHelper = $ds->getOAuth2LoginHelper();
+            $accessToken = $loginHelper->exchangeAuthorizationCodeForToken($code, $realmId);
+            $ds->updateOAuth2Token($accessToken);
+            return ['status'=>true, 'token'=>$accessToken, 'message'=>null];
+        }
+        catch(\Exception $ex)
+        {
+            return ['status'=>false, 'token'=>null, 'message'=>$ex->getMessage()];
+        }
+    }
+
+    /**
+     * Helper function to retrive data service object with settings saved in it
+     * @return DataService
+     * @throws \QuickBooksOnline\API\Exception\SdkException
+     */
+    private static function getDataService()
+    {
+        return DataService::Configure
+        ([
+            'auth_mode' => 'oauth2',
+            'ClientID' => env('QUICKBOOKS_CLIENT_ID'),
+            'ClientSecret' =>  env('QUICKBOOKS_CLIENT_SECRET'),
+            'RedirectURI' => URL::to("quickbooks-response"),
+            'scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
+            'baseUrl' => self::getBasePath()
+        ]);
+    }
+
+    /**
+     * Helper function to get base/rest api path
+     * @return mixed
+     */
+    private static function getBasePath()
+    {
+        if(env('QUICKBOOKS_ENV')=="sandbox"){
+            return env("QUICKBOOKS_SANDBOX_URL");
+        } else {
+            return env("QUICKBOOKS_PRODUCTION_URL");
+        }
+    }
+
+    /**
+     * Helper function to check if we have valid refresh token
+     * @return bool|null
+     */
+    private static function isValidRefreshToken()
+    {
+        $accessToken = Tokens::getTokenInfoByMerchantID(self::$merchantId);
+        if($accessToken)
+        {
+            $timeNow = strtotime(date('Y-m-d H:i:s'));
+            $timeThen = strtotime($accessToken->refresh_token_expiry);
+            $timeResult = $timeThen - $timeNow;
+
+            if($timeResult > 3600 ) {
+                return true;
+            }
+
+            return false;
+        }
+        return null;
+    }
+
+    /**
+     * Helper function to check if we have valid access token
+     * @return bool|null
+     */
+    private static function isValidAccessToken()
+    {
+        $accessToken = Tokens::getTokenInfoByMerchantID(self::$merchantId);
+        if($accessToken)
+        {
+            $timeNow = strtotime(date('Y-m-d H:i:s'));
+            $timeThen = strtotime($accessToken->access_token_expiry);
+            $timeResult = $timeThen - $timeNow;
+
+            if($timeResult > 300 ) {
+                return true;
+            }
+
+            return false;
+        }
+        return null;
+    }
+
+    /**
+     * Helper function to check if we have valid refresh token in db
+     * @return bool
+     */
+    private static function hasAccessAndRefreshTokens()
+    {
+        $accessToken = Tokens::getTokenInfoByMerchantID(self::$merchantId);
+        if($accessToken)
+            return true;
+        return false;
+    }
+
+    /**
+     * Helper function to get access token in various requests
+     * @return array
+     * @throws \Exception
+     */
+    private static function getActiveAccessToken()
+    {
+        if(!self::hasAccessAndRefreshTokens() || !self::isValidRefreshToken())
+            throw new \Exception("Please authenticate quickbooks api from settings panel!");
+
+        try
+        {
+            $ds = self::getDataService();
+            $loginHelper = $ds->getOAuth2LoginHelper();
+            $accessToken = Tokens::getTokenInfoByMerchantID(self::$merchantId);
+
+            if(self::isValidAccessToken()) {
+                return ["status"=>true, "value"=> $accessToken->access_token ];
+            }
+            $accessTokenObj = $loginHelper->refreshAccessTokenWithRefreshToken($accessToken->refresh_token);
+
+            $accessToken->access_token =  $accessTokenObj->getAccessToken();
+            $accessToken->access_token_expiry = $accessTokenObj->getAccessTokenExpiresAt();
+            $accessToken->refresh_token = $accessTokenObj->getRefreshToken();
+            $accessToken->refresh_token_expiry = $accessTokenObj->getRefreshTokenExpiresAt();
+            $accessToken->save();
+
+            return ["status"=>true, "value"=> $accessToken->access_token];
+        }
+        catch(\Exception $ex)
+        {
+            return ["status"=>false, "value"=>$ex->getMessage()];
+        }
+    }
+}
--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,9 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\AccountingDrivers\QuickBooksHelper;
+use App\Tokens;
+use App\Transactions;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 
 class FrontendController extends Controller
 {
-    //
+    /**
+     * Function redirects from root to list page
+     *
+     * @return redirect
+     */
+    public function index()
+    {
+        return redirect(URL::to('list/1000'));
+    }
+
+    /**
+     * Function to list transactions of merchant
+     *
+     * @param integer $merchant_id Merchant id value from url
+     * @return view
+     */
+    public function list($merchant_id)
+    {
+        $transactions = Transactions::where('merchant_code', $merchant_id)->get();
+        return view('index', ['transactions' => $transactions]);
+    }
+
+    /**
+     * Function to trigger quick books authentication to obtain access token
+     *
+     * @return redirect
+     */
+    public function quickBooksAuth()
+    {
+        return redirect(QuickBooksHelper::getAuthUrl());
+    }
+
+    /**
+     * Function to receive code from query string returned by quick books auth
+     *
+     * @param Request $request Code query string value
+     * @return void
+     */
+    public function handleQuickBooksAuth(Request $request)
+    {
+        if(!empty($request->code)) {
+
+            // get access token
+            $accessToken = QuickBooksHelper::exchangeCodeForToken($request->code, env('QUICKBOOKS_REALM_ID'));
+
+            // start storing access token
+            if($accessToken['status'] && $accessToken['token'] != null) {
+
+                // insert token
+                $token = new Tokens;
+                $token->access_token =  $accessToken['token']->getAccessToken();
+                $token->access_token_expiry = $accessToken['token']->getAccessTokenExpiresAt();
+                $token->refresh_token = $accessToken['token']->getRefreshToken();
+                $token->refresh_token_expiry =$accessToken['token']->getRefreshTokenExpiresAt();
+                $token->platform = 'quickbooks';
+                $token->merchant_code = 1000;
+                $token->save();
+
+                // display obtained message
+                echo 'Access token has been obtained successfully! <a href="'.URL::to('/').'">Home</a>';
+                return;
+
+            }
+        }
+
+        echo $accessToken['message'].' <a href="'.URL::to('/auth').'">Retry</a>';
+        return;
+    }
+
+
 }
