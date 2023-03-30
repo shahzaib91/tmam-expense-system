c84843d924bfe04d317d43bbab32ad36409ac4a5
Shahzaib <Shahzaib.2377@gmail.com>
2023-03-30 21:36:25 +0400

    Param input type changed.

--- app/Tokens.php
+++ app/Tokens.php
@@ -10,10 +10,10 @@ class Tokens extends Model
     /**
      * Function takes merchantId as input and return token if present
      *
-     * @param Integer $merchantId
+     * @param integer $merchantId
      * @return App\Tokens
      */
-    public function getTokenInfoByMerchantID(Integer $merchantId)
+    public static function getTokenInfoByMerchantID(int $merchantId)
     {
         return Tokens::where('merchant_code', $merchantId)->first();
     }
